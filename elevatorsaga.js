e50456d407c550bb1468e6e9c891d1504f4cd715
Miller Hederi <millerhederi@gmail.com>
2015-01-31 16:17:12 -0500

    wip

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -4,7 +4,7 @@
 
         getClosestFloorAbove = function(currentFloor) {
             for (var i = currentFloor + 1; i < floors.length; i++) {
-                if (floors[i]._.utime != NIL || floors[i]._.dtime != nil) {
+                if (floors[i]._.utime != NIL || floors[i]._.dtime != NIL) {
                     return i;
                 }
             }
@@ -14,7 +14,7 @@
 
         getClosestFloorBelow = function(currentFloor) {
             for (var i = currentFloor - 1; i > -1; i--) {
-                if (floors[i]._.utime != NIL || floors[i]._.dtime != nil) {
+                if (floors[i]._.utime != NIL || floors[i]._.dtime != NIL) {
                     return i;
                 }
             }
@@ -77,20 +77,44 @@
 
         _.each(elevators, function(elevator) {
             elevator._ = {}
-            elevator._.direction = 0;
 
             elevator.on('idle', function() {
-                if (elevator._.direction == 0) {
+                pressedFloors = elevator.getPressedFloors();
+
+                console.log('---');
+                console.log('Pressed: ', pressedFloors);
+                console.log('Waiting:', 
+                    _.map(
+                        _.where(floors, function(floor) { floor._.utime != NIL || floor._.dtime != NIL}),
+                        function(floor) { return floor.floorNum(); }));
+
+                if (pressedFloors.length > 0) {
+                    optimalFloor = pressedFloors[0];
+                    optimalDelta = Math.abs(optimalFloor - elevator.currentFloor());
+
+                    _.each(pressedFloors, function(floor) {
+                        delta = Math.abs(floor - elevator.currentFloor());
+
+                        if (delta < optimalDelta) {
+                            optimalDelta = delta;
+                            optimalFloor = floor;
+                        }
+                    });
+
+                    elevator.goToFloor(optimalFloor);
+                    floors[optimalFloor]._.utime = NIL;
+                    floors[optimalFloor]._.dtime = NIL;
+                }
+                else {
                     closestFloorDto = getClosestFloor(elevator.currentFloor());
 
                     if (closestFloorDto.direction == 0) {
                         return;
                     }
 
-                    elevator._.direction = closestFloorDto.direction;
-                    updateDirectionIndicator(elevator);
-
                     elevator.goToFloor(closestFloorDto.floor);
+                    floors[closestFloorDto.floor]._.utime = NIL;
+                    floors[closestFloorDto.floor]._.dtime = NIL;
                 }
             });
         });
